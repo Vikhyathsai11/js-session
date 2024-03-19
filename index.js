c05b2cb4ed6865b62f4ea63e702be96fb9f5cc3b
Vikhyathsai11 <vikhyathsai11@gmail.com>
2024-03-19 19:14:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+let a= 10;
+
+var b= 5;
+a=5;
+const x=11;
+const string = "hello";
+const bool=true;
+const nullvalue= null;
+const array =[1,2,3,4,5,"heyy","xyz",true,154,[1,2,3,"xyzzx"]];
+
+
+const object={
+    key:"value",
+    key1:"value1",
+    key2:"value2",
+
+}
+console.log(object.key1)
+
+
+const dosomething = (s) => {
+    console.log("----------------------------hey-----"+s+"----------------viki--");
+}
+dosomething("viki");
+
+
+var age = 18;
+
+if (age >= 18) {
+    console.log("You adult.");
+} else {
+    console.log("Youminor.");
+}
+
+var str1 = "true";
+var str2 = false;
+var str3 = "random";
+
+var bool1 = Boolean(str1);
+var bool2 = Boolean(str2);
+var bool3 = Boolean(str3);
+
+console.log(bool1); 
+console.log(bool2); 
+
+const ar =str1 +1+20*123-23
+console.log(ar)
+
+let v=10;
+if (v=="ten"){
+    console.log("equal");
+}
+else{
+    console.log("not equal");
+}
+if (v===10){
+    console.log("equal")
+}
+
+console.log(true && true);   
+console.log(true && false);  
+console.log(false && true);  
+console.log(false && false); 
+
+console.log(true || true);   
+console.log(true || false);
+
+console.log(!true);  
+console.log(!false); 
+
+var z = 5;
+var y = 10;
+console.log("#########")
+console.log(z < 10 && y > 5); 
+
+console.log(x === 5 || y === 5); 
+
+console.log(!(x === y)); 
+
+
+const sample212 = true ? "hello" : "World";
+
+const test112 = 0 || "placeholder";
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+
